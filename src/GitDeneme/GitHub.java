3b09baae7758c9bf86e67625c71783275e328d97
Mustafa Tanis <mustafatanis97@gmail.com>
2020-11-22 19:19:44 -0500

    Deneme 2 / Mustafa's Way baslangic

--- src/GitDeneme/GitHub.java
+++ src/GitDeneme/GitHub.java
@@ -3,5 +3,7 @@ package GitDeneme;
 public class GitHub {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+
+        System.out.println("SELAMLAAAARRR DENEME 2");
     }
 }
